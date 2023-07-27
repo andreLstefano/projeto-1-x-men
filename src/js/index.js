90b28fb68b8070d4790c50acfa9960faf4811301
andreLstefano <stefano.trabalho@gmail.com>
2023-07-27 18:38:41 -0300

    js
    problema no setor de remove da imagem ...

--- src/js/index.js
+++ src/js/index.js
@@ -4,22 +4,23 @@ personagens.forEach((personagem) => {
     personagem.addEventListener ('mouseenter', () => {
 
 if(window.innerWidth < 450){
-    window.scrollTo({top:0, behavior: 'smooth'});
+    window.scrollTo({top: 0, behavior: 'smooth'});
 }
+        //passo 3 verificar se ja existe um personagem selecionado, se sim, devemos remover a seleçao dele
+        removerSelecaoDePersonagem();
 
-        // removerSelecaoDoPersonagem();
         personagem.classList.add('selecionado');
-        
-        //passo 1
+        //passo 1 pegar elemento do personagem grande para adicionar as informaçoes nele
         alterarImagemPersonagemSelecionado(personagem);
-        //passo 3
+        //passo 3  alterar o nome do personagem grande 
         alterarNomePersonagemSelecionado(personagem);
-        //passo 4
+        //passo 4 alterar a descriçao do personagem grande 
         alterarDescricaoPersonagem(personagem);
 
     })
 
 })
+
 
 function alterarDescricaoPersonagem(personagem) {
     const descricaoPersonagem = document.getElementById('descricao-personagem');
@@ -37,7 +38,7 @@ function alterarImagemPersonagemSelecionado(personagem) {
     imagemPersonagemGrande.src = '.src/imagens/card-${idPersonagem}.png';
 }
 
-function removerSelecaoDoPersonagem() {
+function removerSelecaoDePersonagem() {
     const personagemSelecionado = document.querySelector('.selecionado');
     personagemSelecionado.classList.remove('selecionado');
 }
